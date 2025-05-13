157e97113f00bb9adf80a0f23f743bdff51ffe78
mwagenipecy <percyegno@gmail.com>
2025-05-13 18:43:12 +0300

    fx

--- resources/views/pending-user.blade.php
+++ resources/views/pending-user.blade.php
@@ -5,7 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>eShops</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @vite('resources/css/app.css')
     <script src="//unpkg.com/alpinejs" defer></script>
 
@@ -122,7 +121,6 @@
 
     
 
-    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
