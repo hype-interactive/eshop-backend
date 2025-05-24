7d5584ffcabe1500190064ab8a9527af0286bdc2
mwagenipecy <percyegno@gmail.com>
2025-05-24 14:14:18 +0300

    dashboard

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,6 @@
 
   {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
-  <div class="flex items-center justify-center bg-gray-100">
+  <div class="flex items-center justify-center bg-white ">
     <div class="text-center">
         <h1 class="text-4xl font-extrabold text-blue-900 mb-4  text-blue-600">Welcome Back!</h1>
         <p class="text-lg text-gray-600">Please log in to your account</p>
